0adf08d709b6ef489f6481d8f4afefd42e4d97b5
Shyris Josh <danbollezy@gmail.com>
2025-12-17 19:17:44 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
+<script>
+  const supabase = supabase.createClient(
+    "https://YOUR_PROJECT_ID.supabase.co",
+    "YOUR_PUBLIC_ANON_KEY"
+  );
+</script>
 
