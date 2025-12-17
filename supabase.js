32d54f044761f23e7dd49c3543c6e3f050875889
Shyris Josh <danbollezy@gmail.com>
2025-12-17 19:24:50 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
 <script>
   const supabase = supabase.createClient(
-    "https://YOUR_PROJECT_ID.supabase.co",
-    "YOUR_PUBLIC_ANON_KEY"
+    "https://sb_publishable_bOEdWaGNh7lzgkI53LqHvQ_Da6HeIJy.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9manBjZm9vbHRveGR5cmdvc2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5ODUzNzUsImV4cCI6MjA4MTU2MTM3NX0.Rt7QrKeA0Nok2676OtMC8Jw25cmK881msIsvRcyiTT8"
   );
 </script>
 
